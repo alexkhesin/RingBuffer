60d4e491a74186bbf4045f1c7a926027f4351680
Alex Khesin <alex@khesin.com>
2025-05-04 18:02:11 -0400

    RingBuffer class

--- Sources/RingBuffer/RingBuffer.swift
+++ Sources/RingBuffer/RingBuffer.swift
@@ -1,2 +1,55 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Foundation
+
+class RingBuffer<Element> : BidirectionalCollection  {
+  private var data : [Element] = []
+  private var head: Int = 0
+  private var size: Int = 0
+
+  public init(_ size: Int) {
+    data.reserveCapacity(size)
+    self.size = size
+  }
+
+  public func reset() {
+    data.removeAll(keepingCapacity: true)
+    head = 0
+  }
+
+  public func put(_ val: Element) {
+    if (data.count < size) {
+      data.append(val)
+    } else {
+      data[head] = val
+      head = (head + 1) % size
+    }
+  }
+
+  public func empty() -> Bool { return data.count == 0 }
+  public func count() -> Int { return data.count }
+
+  // Collection methods
+  var startIndex: Int   { return 0 }
+  var endIndex: Int     { return data.count }
+  subscript(index: Int) -> Element {
+    precondition(index < endIndex, "Can't advance beyond endIndex")
+    precondition(index >= startIndex, "Can't advance below startIndex")
+    return data[(head + index) % data.count];
+  }
+  func index(after i: Int) -> Int {
+    precondition(i < endIndex, "Can't advance beyond endIndex")
+    // print("after \(i)")
+    return i + 1
+  }
+  func index(before i: Int) -> Int {
+    precondition(i >= startIndex, "Can't advance below startIndex")
+    // print("before \(i)")
+    return i - 1
+  }
+
+  // Sequence methods
+  /*
+    public func makeIterator() -> RingBufferIterator<Element> {
+      return RingBufferIterator(data, head, data.count)
+    }
+  */
+}
--- Tests/RingBufferTests/RingBufferTests.swift
+++ Tests/RingBufferTests/RingBufferTests.swift
@@ -1,6 +1,94 @@
 import Testing
 @testable import RingBuffer
 
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+class ElC {
+  public var e: Int
+  public init(_ e: Int) { self.e = e }
+}
+
+struct ElS {
+  var e: Int
+  public init(_ e: Int) { self.e = e }
+  mutating func set(_ e: Int) { self.e = e }
+}
+
+struct RingBufferTests {
+  func getAll<Element>(_ rb: RingBuffer<Element>) -> [Element] {
+    var arr = [Element]()
+    for e in rb {
+      arr.append(e)
+    }
+    return arr
+  }
+  
+  func getAllReverse<Element>(_ rb: RingBuffer<Element>) -> [Element] {
+    var arr = [Element]()
+    for e in rb.reversed() {
+      arr.append(e)
+    }
+    return arr
+  }
+  
+  func verify(_ rb: RingBuffer<Int>, _ expected: [Int]) {
+    #expect(getAll(rb) == expected)
+    #expect(getAllReverse(rb) == expected.reversed())
+  }
+  
+  @Test func testPopulate() {
+    let rb = RingBuffer<Int>(3)
+    rb.put(42)
+    verify(rb, [42])
+    rb.put(43)
+    verify(rb, [42, 43])
+    rb.put(44)
+    verify(rb, [42, 43, 44])
+    // This will start pushing out elements since the ring buffer capacity is 3 elements
+    rb.put(45)
+    verify(rb, [43, 44, 45])
+    rb.put(46)
+    verify(rb, [44, 45, 46])
+  }
+  
+  func find(_ rb: RingBuffer<ElC>, _ val:Int) -> ElC? {
+    for e in rb {
+      if e.e == val { return e }
+    }
+    return nil
+  }
+  
+  @Test func testFind() {
+    let rb = RingBuffer<ElC>(3)
+    rb.put(ElC(3))
+    rb.put(ElC(4))
+    rb.put(ElC(5))
+    #expect(getAll(rb)[1].e == 4)
+    find(rb, 4)?.e = 50
+    #expect(getAll(rb)[0].e == 3)
+    #expect(getAll(rb)[1].e == 50)
+    #expect(getAll(rb)[2].e == 5)
+  }
+  
+  @Test func testMutateClass() {
+    let rb = RingBuffer<ElC>(3)
+    rb.put(ElC(3))
+    rb.put(ElC(4))
+    rb.put(ElC(5))
+    #expect(getAll(rb)[1].e == 4)
+    for e in rb {
+      e.e = 1
+    }
+    #expect(getAll(rb)[1].e == 1)
+  }
+  
+  @Test func testMutateStruct() {
+    let rb = RingBuffer<ElS>(3)
+    rb.put(ElS(3))
+    rb.put(ElS(4))
+    rb.put(ElS(5))
+    #expect(getAll(rb)[1].e == 4)
+    for var e in rb {
+      e.set(1)
+    }
+    #expect(getAll(rb)[1].e == 4)
+  }
 }
